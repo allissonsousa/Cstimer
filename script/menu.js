70bd08aa85754229469b4d307f7e6529c8569cdc
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2025-06-19 03:44:45 -0300

    criação boneco puxando artigo

--- script/menu.js
+++ script/menu.js
@@ -1,6 +1,26 @@
+function surgeTempo() {
+  const corpo = document.getElementById("cronometro");
+  const logo = document.getElementById("cubo");
+  const tempos = document.getElementById("anotacaotempo");
+  tempos.style.display = "flex";
+  corpo.style.filter = 'blur(8px)';
+  logo.style.filter = 'blur(8px)';
+}
+
+function fechaArtigo() {
+  const corpo = document.getElementById("cronometro");
+  const logo = document.getElementById("cubo");
+  const artigo = document.getElementById("anotacaotempo");
+  artigo.style.display = "none";
+  corpo.style.filter = 'blur(0px)';
+  logo.style.filter = 'blur(0px)';
+}
+
 
 function abreFecha() {
-    const submenu = document.getElementById("menuItem");
-    submenu.style.animation = 'some 0.4s ease-in-out'
-    submenu.style.display = submenu.style.display === "block" ? "none" : "block";
-  }
+  const submenu = document.getElementById("menuItem");
+  submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+
+}
+
+
--- script/ordem.js
+++ script/ordem.js
@@ -1,23 +1,24 @@
-function ordem(){let cont = 0
-listaFinal = []
-ultimaLetra = ''
-while (cont < 14){
-    const lista1 = [" R "," L ", " F "," D "," B "," U "]
-    const lista2 = ["","´ ","2 "]
-    let item1 = lista1[Math.floor(Math.random() * lista1.length)]
-    let item2 = lista2[Math.floor(Math.random() * lista2.length)]
-    let itemFinal = item1 + item2
-    if (listaFinal.length == 0){
-        listaFinal.push(itemFinal)
+function ordem() {
+    let cont = 0
+    listaFinal = []
+    ultimaLetra = ''
+    while (cont < 14) {
+        const lista1 = [" R ", " L ", " F ", " D ", " B ", " U "]
+        const lista2 = ["", "´ ", "2 "]
+        let item1 = lista1[Math.floor(Math.random() * lista1.length)]
+        let item2 = lista2[Math.floor(Math.random() * lista2.length)]
+        let itemFinal = item1 + item2
+        if (listaFinal.length == 0) {
+            listaFinal.push(itemFinal)
+            ultimaLetra = item1
+            cont += 1
+        } else if (ultimaLetra != itemFinal && ultimaLetra != item1) {
+            listaFinal.push(itemFinal)
+            cont += 1
+        }
         ultimaLetra = item1
-        cont += 1
-    } else if (ultimaLetra != itemFinal && ultimaLetra != item1) {
-        listaFinal.push(itemFinal)
-        cont += 1
-    } 
-    ultimaLetra = item1
-}
+    }
 
-document.getElementById("random").textContent = listaFinal
+    document.getElementById("random").textContent = listaFinal
 }
 
