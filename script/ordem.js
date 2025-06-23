94bd40964c7d43fd900252b5c0bdd17d23cbd4b4
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2025-06-22 23:53:44 -0300

    Adição de comentarios

--- script/ordem.js
+++ script/ordem.js
@@ -1,12 +1,16 @@
+//Esse código entrega uma sequencia aleatoria de 12 simbolos, podendo conter o número 2 ou uma aspas simples 
+//significando q o movimento deve ser antiorario. Nem uma letra deve repetir em sequencia
+
+
 function ordem() {
     let cont = 0
     listaFinal = []
     ultimaLetra = ''
     while (cont < 12) {
-        const lista1 = [" R ", " L ", " F ", " D ", " B ", " U "]
-        const lista2 = ["", "´ ", "2 "]
+        const lista1 = [" R ", " L ", " F ", " D ", " B ", " U "]    //lista de letras
+        const lista2 = ["", "´ ", "2 "]                              //lista de possiveis pares para a letra
         let item1 = lista1[Math.floor(Math.random() * lista1.length)]
-        let item2 = lista2[Math.floor(Math.random() * lista2.length)]
+        let item2 = lista2[Math.floor(Math.random() * lista2.length)]       //seleciona uma letra e um simbolo
         let itemFinal = item1 + item2
         if (listaFinal.length == 0) {
             listaFinal.push(itemFinal)
