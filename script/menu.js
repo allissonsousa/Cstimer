7926dfcc4ec0e008af74e2b8affd76de9f9f5b8d
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-06-26 14:31:06 -0300

    Atualização js

--- script/menu.js
+++ script/menu.js
@@ -1,16 +1,26 @@
 function surgeTempo(){
   const artigo = document.getElementById("anotacaomedia");
-  artigo.classList.add("artigoaparece")
+  if (artigo.style.display == "none"){
+    artigo.classList.add("artigoaparece");
+    artigo.style.display = "flex";
+  } else {
+    artigo.classList.add("artigosome");
+    artigo.style.display = "none";
+  }
 }
+
 
 function fechaArtigo() {
   const artigomedia = document.getElementById("anotacaomedia");
   artigomedia.classList.remove("artigoaparece")
 }
 
+
 function abreFecha() {
   const submenu = document.getElementById("menuItem");
   submenu.style.display = submenu.style.display === "flex" ? "none" : "flex";
+  console.log('tudo certo');
+  
 
 }
 
